49fd2102d17d7d072b6a3fb7ff75aac8c6cb985d
Vasilescu Andrei <vasilescuandrei24@gmail.com>
2025-03-17 16:14:52 +0200

    wip flash loan endpoint + config endpoint

--- src/flash_loan.rs
+++ src/flash_loan.rs
@@ -19,8 +19,26 @@ pub trait FlashLoan {
         loan_token_id: &EgldOrEsdtTokenIdentifier,
         amount: BigUint,
         loan_receiver_contract_addr: &ManagedAddress,
-        receiver_contract_endpoint: &ManagedBuffer<Self::Api>,
+        receiver_contract_endpoint: ManagedBuffer<Self::Api>,
         args: ManagedArgBuffer<Self::Api>,
     ) {
+        require!(
+            amount > BigUint::from(0u128),
+            "Loaned amount must be greater than 0"
+        );
+        self.check_contract_shard(loan_receiver_contract_addr);
+    }
+
+    #[endpoint(flashLoanConfig)]
+    #[only_owner]
+    fn flash_loan_config(&self) {}
+
+    fn check_contract_shard(&self, contract_addr: &ManagedAddress) {
+        let my_contract_addr = self.blockchain().get_sc_address();
+        require!(
+            self.blockchain().get_shard_of_address(contract_addr)
+                == self.blockchain().get_shard_of_address(&my_contract_addr),
+            "Contract is not in the same shard"
+        );
     }
 }
